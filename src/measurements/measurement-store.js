8749213643edd607bdacae222625c18205c8e7b9
Branden <b.lowe333@gmail.com>
2019-07-10 22:05:17 -0700

    Implement add measurement route logic, data store.

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -1,12 +1,23 @@
 import { Measurement } from './measurement';
 import { HttpError } from '../errors';
 
+// NOTE:
+//  Chose to use a map over an array or object
+//    1) Achieve O(1) look up times for a single record. Would be O(n) for an array
+//    2) Query date range function could be optimized.
+//        - If records are inserted sequentially,then we can stop looking thru map
+//          once 'to' value has been found. Map guarantees
+//          insertion order and objects do not.
+const store = new Map();
+
 /**
  * Add new measurement
  * @param {Measurement} measurement to be added
  */
 export function add(measurement) {
-  throw new HttpError(501);
+  if (measurement.metrics.size) {
+    store.set(measurement.timestamp.toISOString(), measurement);
+  }
 }
 
 /**
